d3259c78544b3b75c8bd43bd9e0cd502cb5c3f2c
Unknown <matheus.medeiros.702@LA10212017584L.imd.local>
2024-03-11 18:10:32 -0300

    Aula 5 pratica

---
+++ src/java-aulas/Prova.java
@@ -0,0 +1,37 @@
+
+public class Prova{
+    private double notaParte1;
+    private double notaParte2;
+
+    public Prova(){
+        notaParte1 = 0.0;
+        notaParte2 = 0.0;
+    }
+
+    public double calcularNotaTotal(){
+        double soma = 0.0;
+        soma = soma + this.notaParte1 + this.notaParte2;
+        
+        if (soma > 10){
+            soma = 10.0;
+        }
+        
+        return soma;
+    }
+
+    public void setNotaParte1(double nota){
+        this.notaParte1 = nota;
+    }
+
+    public double getNotaParte1(){
+        return this.notaParte1;
+    }
+
+    public void setNotaParte2(double nota){
+        this.notaParte2 = nota;
+    }
+
+    public double getNotaParte2(){
+        return this.notaParte2;
+    }
+}
---
+++ src/java-aulas/Turma.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Turma{
+    private ArrayList<Aluno> alunos;
+
+    public Turma(){
+        alunos = new ArrayList<Aluno>();
+    }
+
+    public void adicionarAlunoTurma(Aluno aluno){
+        alunos.add(aluno);
+    }
+
+    public int qtdAlunoTurma(){
+        return alunos.size();
+    }
+
+    public double calcularMediaTurma(){
+        double somaMediasAlunos = 0.0;
+        double mediaTurma = 0.0;
+
+        for(Aluno aluno : alunos) {
+            
+            somaMediasAlunos = somaMediasAlunos + aluno.calcularMedia();
+
+        }
+
+        return mediaTurma = somaMediasAlunos/qtdAlunoTurma(); 
+    }
+
+}
